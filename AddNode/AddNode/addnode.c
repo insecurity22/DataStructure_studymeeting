ebfddc2421a8ab1738c951853dfb95fa5fa896bc
bogyeong <insecurity22@naver.com>
2019-07-29 14:42:00 +0900

    Add AddNode

---
+++ AddNode/AddNode/addnode.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+typedef struct Node {
+	int data;
+	struct Node* next;
+} Node;
+
+Node* MakeNode(int data) {
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->next = NULL;
+	node->data = data;
+
+	return node;
+}
+
+Node* AddNode(Node* head, int data) {
+	Node* temp = head;
+	Node* new = MakeNode(data); // 데이터 계속해서 만들어줌
+	while (temp->next != NULL) {
+		temp = temp->next;
+	}
+	head = head->next;
+}
+
+int main() {
+
+}
